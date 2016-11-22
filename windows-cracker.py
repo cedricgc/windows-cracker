f07cc265f7606343a62bf332979b973d19738bbe
root <root@localhost.localdomain>
2016-11-21 20:11:51 -0500

    Added some helper method stubs.

---
+++ windows-cracker.py
@@ -0,0 +1,11 @@
+# Helper methods
+
+# input: the std.out results of running a crackmap SAM grab
+# output: The correctly formatted
+def formatCrackmapSAM(input):
+
+# input: the std.out results of running nmap -O on a given IP range
+# output: a list containing the IP addresses (as strings) of any machines nmap thinks are windows
+def formatNmapO(input):
+
+
